5b1efea55799943a040dc72594c91347747409e6
AndroidBiscuit <faizanahmad9619@gmail.com>
2024-07-17 17:05:11 -0400

    Added main files and test code

--- A1/main.cpp
+++ A1/main.cpp
@@ -2,3 +2,17 @@
 // Assignment 1
 // Faizan Ahmad (40100581)
 
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    // Test code
+    cout << "Hello World!" << endl;
+
+    // Return statement
+    return 0;
+}
