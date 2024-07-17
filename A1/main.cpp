a342e5d31a8c4ca4290a8dce1c38e01af8384740
AndroidBiscuit <faizanahmad9619@gmail.com>
2024-07-17 17:18:05 -0400

    Added structure for part 1

--- A1/main.cpp
+++ A1/main.cpp
@@ -9,9 +9,36 @@
 
 using namespace std;
 
+static void createArray(int size) {
+    //
+}
+
+static void initializeArray(int* pointer, int size) {
+    //
+}
+
+static void printArray(int* pointer, int size) {
+    //
+}
+
+static void deleteArray(int* pointer) {
+    //
+}
+
+static void partOne() {
+    cout << "\n-*-*-*-*-*-   Part 1 - Array manipulation   -*-*-*-*-*-\n" << endl;
+
+    // 1.1 - Create dynamic array
+
+    // 1.2 - Print array
+
+    // 1.3 - Delete array
+}
+
 int main(){
-    // Test code
-    cout << "Hello World!" << endl;
+    // =================================================================
+    // Part 1: Array manipulation
+
 
     // Return statement
     return 0;
